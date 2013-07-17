e7e0640bc1bc078ec4a44e6175b0fb68ec6fc966
Dan Manchester <dan@needmoredesigns.com>
2013-07-16 22:33:31 -0700

    Homepage template (first pass)

---
+++ front-page.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+  * homepage template
+  */
+
+get_header();
+
+get_template_part( 'templates/home', 'issue' );
+
+?><div class="row">
+  <hr class="rule  rule--ornament rule--spaced" data-ornament="§">
+</div><?php
+?><div class="row">
+    
+    <div class="large-8 large-centered columns"><img src="http://placehold.it/800x100" alt="ad"></div>
+
+  </div><?php
+
+get_template_part( 'templates/home', 'widgets' );
+
+get_footer();
