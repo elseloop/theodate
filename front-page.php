fa94cf878f9416c97bbdded28fdeb503cce90bad
Dan Manchester <dan@needmoredesigns.com>
2013-07-22 22:25:32 -0700

    Cleaning

--- front-page.php
+++ front-page.php
@@ -9,8 +9,11 @@ get_header();
 get_template_part( 'templates/home', 'issue' );
 
 ?><div class="row">
+
   <hr class="rule  rule--ornament rule--spaced" data-ornament="§">
+
 </div><?php
+
 ?><div class="row">
     
     <div class="large-8 large-centered columns"><img src="http://placehold.it/800x100" alt="ad"></div>
